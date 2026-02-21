a3a586752f849a82c4d14bb3cefd2f05ae974386
lucasfreitaas <lucasfreitasfe05@gmail.com>
2026-02-21 13:28:16 -0300

    Primeiro commit: Criação de Banco de Dados no MYSQL via Docker e configuração inicial de dependências.

---
+++ src/test/java/com/lucasfreitaas/payment_system/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasfreitaas.payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
